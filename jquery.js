562575f1c0b7f3ba7f3474e5894d1144548eb57a
Trevor McKee <trevorjmckee@wustl.edu>
2021-04-16 20:36:18 -0500

    Add .txt file for game-list and function for switching out upper list item

--- jquery.js
+++ jquery.js
@@ -6,4 +6,11 @@ $(document).ready(function() {
     $("button").click(function() {
         $(".game-section-container").load("game-content.txt #hollow-knight");
     });
+
+    $(".nav-type-one-gradient-top").click(function() {
+        $(".nav-top-entry").toggleClass("nav-top-entry-transition").delay(1000).load("game-list-entries.txt #your-index").toggleClass("nav-top-entry-transition");
+        
+        // $(".nav-top-entry").load("game-list-entries.txt #your-index");
+        // $(".nav-top-entry").toggleClass("nav-top-entry-transition");
+    })
 })
